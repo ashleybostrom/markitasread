b169006858907480c6fe98f43fefcba2cbf91e79
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-16 15:39:41 -0700

    require util for path

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util')
 const generateMarkdown = require('./utils/generateMarkdown');
 
 console.log('Welcome to the README.md Generator!');
