61449b2d3facf99a434521ddbec0c17758f43bc7
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-16 15:52:59 -0700

    tried to run and need to debug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util')
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
 
 console.log('Welcome to the README.md Generator!');
 
