49a067c3a114e240ea47a7df8a9bc9b4c951b8bc
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-20 13:57:37 -0700

    reformat generatemarkdown to use data from indexjs

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,39 @@
+  // License info
+
+    function renderLicenseBadge(licenseInput) {
+
+      if (responses.licenseInput = 'MIT License') {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+      }
+      else if (responses.licenseInput = 'GNU GPL v3') {
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+      }
+      else if (responses.licenseInput = 'Apache License 2.0') {
+        return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+      } 
+      else if (responses.licenseInput = 'ISC License (ISC)') {
+        return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+      }
+      else if (responses.licenseInput = 'Mozilla Public License 2.0') {
+        return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+      } 
+      else if (responses.licenseInput = 'IBM Public License Version 1.0') {
+        return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`
+      }
+    }
+
 // function to generate markdown for README
-var licenseUsed = ''
-
-function generateMarkdown(responses) {
-
-  let markDown = `
-  
+function generateMarkdown(data) {
+  return (`
   # ${responses.titleInput}
   
-
   ## Description
 
   *The purpose of this project and what it achieves:*
 
   ${responses.descriptionInput}
 
-  
-  `
-  markDown +=
-
-
-    `## Table of Contents
+  ## Table of Contents
  
   * [Installation](#installation)
 
@@ -29,91 +43,38 @@ function generateMarkdown(responses) {
 
   * [Credits](#credits)
   
-  * [Questions](#questions)`
-
-    // Installation
-  if (responses.installationInput !== '') {
-
-    markDown += `
+  * [Questions](#questions)
   
   ## Installation
   
   *Steps needed to install and run the application:*
   
-  ${responses.installationInput}`
-  };
+  ${responses.installationInput}
 
-    // Usage
-  if (responses.usageInput !== '') {
+  ## Usage
 
-    markDown += `
-  
-  ## Usage 
-  
   *Instructions and potential use cases:*
   
-  ${responses.usageInput}`
-  };
+  ${responses.usageInput}
   
-    // License info
-  if (responses.licenseInput = 'MIT License') {
-    licenseUsed = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-  }
-  else if (responses.licenseInput = 'GNU GPL v3') {
-    licenseUsed = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-  }
-  else if (responses.licenseInput = 'Apache License 2.0') {
-    licenseUsed == `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-  } 
-  else if (responses.licenseInput = 'ISC License (ISC)') {
-    licenseUsed = `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
-  }
-  else if (responses.licenseInput = 'Mozilla Public License 2.0') {
-    licenseUsed = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
-  } 
-  else if (responses.licenseInput = 'IBM Public License Version 1.0') {
-    licenseUsed = `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`
-
-
-  };
-  markDown +=
-    `
+  ## Licensing
   
-  ## License
-  
-  * ${licenseUsed}
-  `;
+  ${renderLicenseBadge(data.licenseInput)}
 
-  // Credits
-  if (responses.creditsInput !== '') {
-
-    markDown += `
-  
   ## Credits
   
   *Please credit any contributors or cited sources here.*
   
-  ${responses.creditsInput}`
-  };
+  ${responses.creditsInput}
 
-  // Questions / About Developer section
-  if (responses.githubUsername !== '') {
-
-    markDown += `
-   
-  ## Questions?
+  ## Questions
    
   *For any questions, contact information is below:*
  
-  GitHub: www.github.com/${responses.githubUsername}
+  GitHub: www.github.com/`+`${responses.githubUsername} \n
     
   Email: ${responses.emailAddress}
-  `;
-
-    // Return markdown
-    return markDown;
-
-  };
-};
+  `);
+}
 
 module.exports = generateMarkdown;
